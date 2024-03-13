2d74aec097e441fa33cfa4a84a7f11f1693ac932
Lucas Saliés Brum <sistematico@gmail.com>
2024-03-13 09:36:05 -0400

    Changed BASE_PATH

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 			pages: 'build'
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
